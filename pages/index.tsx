0b8379b24faed92a7a3466964cfd2d44ee437dac
Seth Addo <addoseth35@gmail.com>
2022-09-24 20:49:26 +0000

    finished with about section

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,15 @@
-/* eslint-disable react/no-unescaped-entities */
 import type { NextPage } from 'next';
 import Head from 'next/head';
+import About from '../components/About';
+import Experience from '../components/Experience';
 import Header from '../components/Header';
 import Hero from '../components/Hero';
-// import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
   return (
     <div className='bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-scroll z-0'>
       <Head>
-        <title>Seth's Portfolio</title>
+        <title>Seth&apos;s Portfolio</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
@@ -17,13 +17,19 @@ const Home: NextPage = () => {
       <Header/>
 
       {/* Hero */}
-      <section id='hero' className='snap-center'>
+      <section id='hero' className='snap-start'>
         <Hero/>
       </section>
 
       {/* About */}
+      <section id='about' className='snap-center'>
+        <About />
+      </section>
 
       {/* Experience */}
+      <section id='experience' className='snap-center'>
+        <Experience />
+      </section>
 
       {/* Skills */}
 
