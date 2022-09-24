73a3b187b205c8e6008bb8d1fd67b7c79b23e8e1
Seth Addo <addoseth35@gmail.com>
2022-09-24 20:09:59 +0000

    finished navbar and hero section

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,15 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Header from '../components/Header'
+/* eslint-disable react/no-unescaped-entities */
+import type { NextPage } from 'next';
+import Head from 'next/head';
+import Header from '../components/Header';
+import Hero from '../components/Hero';
 // import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
   return (
-    <div>
+    <div className='bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-scroll z-0'>
       <Head>
-        <title>Seth Addo - Portfolio</title>
+        <title>Seth's Portfolio</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
@@ -15,6 +17,9 @@ const Home: NextPage = () => {
       <Header/>
 
       {/* Hero */}
+      <section id='hero' className='snap-center'>
+        <Hero/>
+      </section>
 
       {/* About */}
 
@@ -27,7 +32,7 @@ const Home: NextPage = () => {
       {/* Contact Me */}
 
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
