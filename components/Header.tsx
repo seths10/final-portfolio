405846ddf45db2d02c33b956e393fdabd00f7318
Seth Addo <addoseth35@gmail.com>
2022-09-25 15:49:48 +0000

    finished with UI

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { SocialIcon } from 'react-social-icons';
 import { motion } from 'framer-motion';
+import Link from 'next/link';
 
 type Props = {};
 
-export default function Header({}: Props) {
+export default function Header({ }: Props) {
 	return (
 		<header className="sticky top-0 p-5 flex items-start justify-between max-w-7xl mx-auto z-20 xl:items-center">
 			<motion.div
@@ -18,9 +19,9 @@ export default function Header({}: Props) {
 					opacity: 1,
 					scale: 1,
 				}}
-        transition={{
-          duration: 1.5,
-        }}
+				transition={{
+					duration: 1.5,
+				}}
 				className="flex flex-row items-center"
 			>
 				{/* Social Icons */}
@@ -41,32 +42,34 @@ export default function Header({}: Props) {
 				/>
 			</motion.div>
 
-			<motion.div
-      initial={{
-        x: 500,
-        opacity: 0,
-        scale: 0.5,
-      }}
-      animate={{
-        x: 0,
-        opacity: 1,
-        scale: 1,
-      }}
-      transition={{
-        duration: 1.5,
-      }}
-      className="flex flex-row items-center text-gray-300 cursor-pointer"
-      >
-				<SocialIcon
-					className="cursor-pointer"
-					network="email"
-					fgColor="gray"
-					bgColor="transparent"
-				/>
-				<p className="uppercase hidden md:inline-flex text-sm text-gray-400">
-					Get In Touch
-				</p>
-			</motion.div>
+			<Link href='#contact'>
+				<motion.div
+					initial={{
+						x: 500,
+						opacity: 0,
+						scale: 0.5,
+					}}
+					animate={{
+						x: 0,
+						opacity: 1,
+						scale: 1,
+					}}
+					transition={{
+						duration: 1.5,
+					}}
+					className="flex flex-row items-center text-gray-300 cursor-pointer"
+				>
+					<SocialIcon
+						className="cursor-pointer"
+						network="email"
+						fgColor="gray"
+						bgColor="transparent"
+					/>
+					<p className="uppercase hidden md:inline-flex text-sm text-gray-400">
+						Get In Touch
+					</p>
+				</motion.div>
+			</Link>
 		</header>
 	);
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
+import Link from 'next/link';
 import About from '../components/About';
 import ContactMe from '../components/ContactMe';
 import Experience from '../components/Experience';
@@ -7,10 +8,11 @@ import Header from '../components/Header';
 import Hero from '../components/Hero';
 import Projects from '../components/Projects';
 import Skills from '../components/Skills';
+import { HomeIcon } from '@heroicons/react/24/solid';
 
 const Home: NextPage = () => {
   return (
-    <div className='bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-scroll z-0'>
+    <div className='bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-y-scroll overflow-x-hidden z-0 scrollbar scrollbar-track-gray-400/5 scrollbar-thumb-[#F7AB0A]/80'>
       <Head>
         <title>Seth&apos;s Portfolio</title>
         <link rel="icon" href="/favicon.ico" />
@@ -38,9 +40,17 @@ const Home: NextPage = () => {
         <Projects />
       </section>
 
-      <section id='contact-me' className='snap-center'>
+      <section id='contact' className='snap-start'>
         <ContactMe />
       </section>
+
+      <Link href='#hero'>
+        <footer className='sticky bottom-5 w-full cursor-pointer'>
+          <div className='flex items-center justify-center'>
+            <HomeIcon className='h-8 w-8 animate-pulse' />
+          </div>
+        </footer>
+      </Link>
 
     </div>
   );
