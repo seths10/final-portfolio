aac3ae6f46d8032c92654f42dc7e6dfd5b9bea76
Seth Addo <addoseth35@gmail.com>
2022-09-28 19:16:17 +0000

    added project id

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -21,7 +21,9 @@ export default function Projects({ projects }: Props) {
 
       <div className='relative w-full flex overflow-x-scroll scrollbar-track-gray-400/5 scrollbar-thin scrollbar-thumb-[#F7AB0A]/80 overflow-y-hidden snap-x snap-mandatory z-20'>
         {projects?.map((project, i) => (
-          <div key={project._id} className='w-screen flex-shrink-0 snap-center flex flex-col space-y-5 items-center justify-center
+          <div
+            key={project._id}
+            className='w-screen flex-shrink-0 snap-center flex flex-col space-y-5 items-center justify-center
            p-20 md:p-44 h-screen'>
             <motion.img
               initial={{ y: -300, opacity: 0 }}
