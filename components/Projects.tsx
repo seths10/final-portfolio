6164e0fbc424fe5c6e6a22aaa7c20fe386e44fe9
Seth Addo <addoseth35@gmail.com>
2022-09-29 21:57:06 +0000

    Done

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -26,7 +26,6 @@ export default function Projects({ projects }: Props) {
             className='w-screen flex-shrink-0 snap-center flex flex-col space-y-5 items-center justify-center
            p-20 md:p-44 h-screen'>
             <motion.img
-              key={project._id}
               initial={{ y: -300, opacity: 0 }}
               transition={{ duration: 1.2 }}
               whileInView={{ opacity: 1, y: 0 }}
--- sanity.ts
+++ sanity.ts
@@ -5,7 +5,7 @@ export const config = {
     dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || "production",
     projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
     apiVersion:"2021-03-25",
-    useCdn: process.env.NODE_NV === "production",
+    useCdn: process.env.NODE_ENV === "production",
 };
 
 export const sanityClient = createClient(config);
